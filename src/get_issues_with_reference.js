df4ad037cca6e6815eea03f5a1a6be0c9b9f9263
iggy_lint <iggy@transposit.com>
2022-02-17 21:53:05 +0000

    iggy fixed your formatting with Prettier

--- src/get_issues_with_reference.js
+++ src/get_issues_with_reference.js
@@ -1,23 +1,28 @@
 function run(params) {
-  var results = api.run("this.list_issues_for_user", {owner: params.owner});
-  for (var i = 0; i < results.length; i++) {   
-  	var reference = results[i].full_name + "#" + results[i].number;
-  	var id = api.query("select id from this.get_airtable_records where reference='"+reference+"'", {baseID: params.baseID, table: params.table});
+  var results = api.run("this.list_issues_for_user", { owner: params.owner });
+  for (var i = 0; i < results.length; i++) {
+    var reference = results[i].full_name + "#" + results[i].number;
+    var id = api.query(
+      "select id from this.get_airtable_records where reference='" +
+        reference +
+        "'",
+      { baseID: params.baseID, table: params.table }
+    );
     var obj = {
-                baseID: params.baseID, 
-                        table: params.table,
-                        reference: reference,
-			title: results[i].title,
-                        state: results[i].state,
-          author: results[i].author,
-                        type: 'issue',
-                        comments: results[i].comments,
-			url: results[i].url,
-                        updated: results[i].updated,
-                        created: results[i].created,
-                        completed: results[i].completed,
-			repo: results[i].name,
-                        };
+      baseID: params.baseID,
+      table: params.table,
+      reference: reference,
+      title: results[i].title,
+      state: results[i].state,
+      author: results[i].author,
+      type: "issue",
+      comments: results[i].comments,
+      url: results[i].url,
+      updated: results[i].updated,
+      created: results[i].created,
+      completed: results[i].completed,
+      repo: results[i].name,
+    };
     if (id.length > 0) {
       results[i].airtable_id = id[0].id;
       obj.recordID = id[0].id;
@@ -32,8 +37,8 @@ function run(params) {
     results[i].reference = reference;
   }
   return {
-    results
-};
+    results,
+  };
 }
 /*
  * For sample code and reference material, visit
